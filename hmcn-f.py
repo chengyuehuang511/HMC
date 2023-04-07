cf6782aeac903742c92b0dcd56f1ce59ed139282
v-chenghuang <v-chenghuang@microsoft.com>
2023-04-07 17:15:18 +0800

    No hierarchy info (but with level info)

--- hmcn-f.py
+++ hmcn-f.py
@@ -1,9 +1,14 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import argparse
 import numpy as np
+from sklearn.metrics import accuracy_score
+from tqdm import tqdm
 
 
 class Dense(nn.Module):
+    """linear + activation"""
     def __init__(self, input_dim, output_dim, activation):
         super().__init__()
         self.input_dim = input_dim
@@ -20,7 +25,7 @@ class Dense(nn.Module):
 
 
 class HMCNFModel(nn.Module):
-    def __init__(self, features_size, label_size, hierarchy, hidden_size=384, beta=0.5, dropout_rate=0.1):
+    def __init__(self, features_size, label_size, hierarchy, hidden_size=384, beta=0.5, dropout_rate=0.1, if_global=True):
         """
         feature_size == x.shape[1]
         label_size == sum(hierarchy)
@@ -32,12 +37,13 @@ class HMCNFModel(nn.Module):
         self.hidden_size = hidden_size
         self.beta = beta
         self.dropout_rate = dropout_rate
+        self.if_global = if_global
 
         def local_model(input_dim, hidden_dim, num_labels, dropout_rate):
             return nn.Sequential(
                 Dense(input_dim, hidden_dim, activation=nn.ReLU()),
                 nn.Dropout(dropout_rate),
-                Dense(hidden_dim, num_labels, activation=nn.Sigmoid())
+                Dense(hidden_dim, num_labels, activation=nn.Softmax(dim=-1))
             )
 
         def global_model(input_dim, output_dim, dropout_rate):
@@ -54,7 +60,10 @@ class HMCNFModel(nn.Module):
                 self.global_models.append(global_model(features_size + hidden_size, hidden_size, dropout_rate))
         
         self.local_models = nn.ModuleList([local_model(hidden_size, hidden_size, hierarchy[i], dropout_rate) for i in range(len(hierarchy))])
-        self.sigmoid_model = Dense(hidden_size, label_size, activation=nn.Sigmoid())
+        
+        if if_global:
+            self.linear_after_global = nn.Linear(hidden_size, label_size)
+            self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, x):
         assert x.shape[-1] == self.features_size
@@ -66,27 +75,49 @@ class HMCNFModel(nn.Module):
                 global_models.append(self.global_models[i](x))
             else:
                 global_models.append(self.global_models[i](torch.cat([global_models[i - 1], x], dim=1)))
-        p_glob = self.sigmoid_model(global_models[-1])
 
         for i in range(len(hierarchy)):
             local_models.append(self.local_models[i](global_models[i]))
 
         p_loc = torch.cat(local_models, dim=1)
-        labels = (1-self.beta) * p_glob + self.beta * p_loc
-        return labels
+
+        if self.if_global:
+            p_glob = self.linear_after_global(global_models[-1])
+            cum = 0
+            for i in range(len(hierarchy)):
+                p_glob[:, cum: cum + hierarchy[i]] = self.softmax(p_glob[:, cum: cum + hierarchy[i]])
+                cum += hierarchy[i]
+            labels = (1-self.beta) * p_glob + self.beta * p_loc
+            return labels
+        return p_loc
 
 
 if __name__=='__main__':
 
-    x = torch.cat([torch.zeros([1000,77]), torch.ones([628,77])], dim=0)
-    y = torch.cat([torch.zeros([1000,499]), torch.ones([628,499])], dim=0)
+    # x = torch.cat([torch.zeros([1000,77]), torch.ones([628,77])], dim=0)
+    # y = torch.cat([torch.zeros([1000,499]), torch.ones([628,499])], dim=0)
 
     # (1628, 77)
     # (1628, 499)
 
+    
+    torch.manual_seed(1)  # cpu
+    torch.cuda.manual_seed(1)  # gpu
+
+    sample_size = 1628
+    feature_size = 77
     hierarchy = [18, 80, 178, 142, 77, 4]
-    feature_size = x.shape[1]
-    label_size = y.shape[1]
+    label_size = sum(hierarchy)
+
+    x = torch.rand(sample_size, feature_size)
+
+    y = []
+    for i in range(len(hierarchy)):
+        targets = torch.randint(0, hierarchy[i], (sample_size, 1))
+        # targets_to_one_hot = F.one_hot(targets, num_classes=hierarchy[i])
+        y.append(targets)
+    y = torch.cat(y, dim=1)
+
     beta = 0.5
     model = HMCNFModel(features_size=feature_size, 
                        label_size=label_size, 
@@ -96,24 +127,31 @@ if __name__=='__main__':
                        dropout_rate=0.1)
     y_pred = model(x)
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
-    criterion = nn.MSELoss()
+    criterion = nn.NLLLoss()
     
-    for t in range(100):
-        # Forward pass: Compute predicted y by passing x to the model
+    for t in range(50):
         y_pred = model(x)
-        # print(y, y_pred)
-        # Compute and print loss
-        loss = criterion(y_pred, y)
-        if t % 10 == 9:
-            print(t, loss.item())
+        loss = []
+        cum = 0
+        for i, h_len in enumerate(hierarchy):
+            loss.append(criterion(torch.log(y_pred[:, cum: cum + h_len]), y[:, i].long()))
+            cum += h_len
+        loss = torch.stack(loss).mean()
+        
+        y_hat = []
+        cum = 0
+        for i in range(len(hierarchy)):
+            y_hat.append(torch.argmax(y_pred[:, cum: cum + hierarchy[i]], dim=1).reshape(sample_size, 1))
+            cum += hierarchy[i]
+        y_hat = torch.cat(y_hat, dim=1)
+        
+        acc = []
+        for i in range(len(hierarchy)):
+            acc.append(accuracy_score(y[:, i].numpy(), y_hat[:, i].numpy()))
+        acc_mean = np.mean(acc)
+        
+        print("Epoch {} | Loss: {} | Mean Acc: {}".format(t, loss.item(), acc_mean))
 
-        # Zero gradients, perform a backward pass, and update the weights.
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    
-    y_predict = torch.where(y_pred > 0.5, 1, 0)
-    y = y.numpy()
-    y_predict = y_predict.numpy()
-    predict_ok = np.where(np.sum(y_predict - y, axis=1) == 0, 1, 0)
-    print("{} good out of {} samples".format(np.sum(predict_ok), predict_ok.shape[0]))
